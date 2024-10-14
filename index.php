ef4b9d12e97221551d7081778e174ba6a6a7421a
emilyrhee <emilyrhee99@gmail.com>
2024-10-13 21:54:52 -0400

    Added text for images

--- index.php
+++ index.php
@@ -18,14 +18,24 @@
 
   <div class="container">
     <div class="row gy-5">
-      <div class="col-sm ratio ratio-1x1">
-        <img class="p-<?= $padding ?>" src="https://www.maangchi.com/wp-content/uploads/2007/11/kimchijjigae-590x411.jpg" style="object-fit: cover;" alt="Kimchi soup">
+      <div class="col-sm">
+        <div class="ratio ratio-1x1">
+          <img class="p-<?= $padding ?>" src="https://www.maangchi.com/wp-content/uploads/2007/11/kimchijjigae-590x411.jpg" style="object-fit: cover;" alt="Kimchi soup">
+        </div>
+        <h6 class="text-center mt-2">Kimchi Soup</h6>
       </div>
-      <div class="col-sm ratio ratio-1x1">
-        <img class="p-<?= $padding ?>" src="https://balancewithjess.com/wp-content/uploads/2022/08/Jumeokbap-Feat.jpg" style="object-fit: cover;" alt="Jumeokbap">
+      <div class="col-sm">
+        <div class="ratio ratio-1x1">
+          <img class="p-<?= $padding ?>" src="https://balancewithjess.com/wp-content/uploads/2022/08/Jumeokbap-Feat.jpg" style="object-fit: cover;" alt="Jumeokbap">
+        </div>
+        <h6 class="text-center mt-2">Rice Balls</h6>
       </div>
-      <div class="col-sm ratio ratio-1x1">
-        <img class="p-<?= $padding ?>" src="https://tiffycooks.com/wp-content/uploads/2021/04/Screen-Shot-2021-04-07-at-1.01.41-AM.png" style="object-fit: cover;" alt="Jumeokbap">
+      <div class="col-sm">
+        <div class="ratio ratio-1x1">
+          <img class="p-<?= $padding ?>" src="https://tiffycooks.com/wp-content/uploads/2021/04/Screen-Shot-2021-04-07-at-1.01.41-AM.png" style="object-fit: cover;" alt="Jumeokbap">
+        </div>
+        <h6 class="text-center mt-2">Marinated Eggs</h6>
+      </div>
       </div>
     </div>
     <div class="row gy-5">
