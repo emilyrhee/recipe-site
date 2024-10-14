4f91bca9d95d32ff40041275e0a2e656351bd0af
emilyrhee <emilyrhee99@gmail.com>
2024-10-13 21:42:01 -0400

    added imgs of food to home page

--- index.php
+++ index.php
@@ -11,7 +11,28 @@
 </head>
 <body>
   <?php include "components/navbar.php"; ?>
-  
+
+  <?php
+    $padding = 3;
+  ?>
+
+  <div class="container">
+    <div class="row gy-5">
+      <div class="col-sm ratio ratio-1x1">
+        <img class="p-<?= $padding ?>" src="https://www.maangchi.com/wp-content/uploads/2007/11/kimchijjigae-590x411.jpg" style="object-fit: cover;" alt="Kimchi soup">
+      </div>
+      <div class="col-sm ratio ratio-1x1">
+        <img class="p-<?= $padding ?>" src="https://balancewithjess.com/wp-content/uploads/2022/08/Jumeokbap-Feat.jpg" style="object-fit: cover;" alt="Jumeokbap">
+      </div>
+      <div class="col-sm ratio ratio-1x1">
+        <img class="p-<?= $padding ?>" src="https://tiffycooks.com/wp-content/uploads/2021/04/Screen-Shot-2021-04-07-at-1.01.41-AM.png" style="object-fit: cover;" alt="Jumeokbap">
+      </div>
+    </div>
+    <div class="row gy-5">
+      <!-- add more imgs here! -->
+    </div>
+  </div>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
