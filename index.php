1c4ef995fa6f262adfbf708fceafc6e57b211ef6
emilyrhee <emilyrhee99@gmail.com>
2024-10-21 13:08:28 -0400

    added navigation buttons and "Filter by" checkboxes

--- index.php
+++ index.php
@@ -5,42 +5,36 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>EasyBytes</title>
   <style>
-    <?php include "styles/styles.css"?>
+    <?php include "styles/styles.css" ?>
   </style>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
   <?php include "components/navbar.php"; ?>
-  <?php
-    $padding = 3;
-  ?>
 
   <div class="container">
-    <div class="row gy-5">
-      <div class="col-sm">
-        <div class="ratio ratio-1x1">
-          <img class="p-<?= $padding ?>" src="https://www.maangchi.com/wp-content/uploads/2007/11/kimchijjigae-590x411.jpg" style="object-fit: cover;" alt="Kimchi soup">
-        </div>
-        <h6 class="text-center mt-2">Kimchi Soup</h6>
+    <div class="row">
+      <div class="col-2">
+        <button class="nav-buttons">My Account</button>
+        <button class="nav-buttons">Subscribe</button>
       </div>
 
-      <div class="col-sm">
-        <div class="ratio ratio-1x1">
-          <img class="p-<?= $padding ?>" src="https://balancewithjess.com/wp-content/uploads/2022/08/Jumeokbap-Feat.jpg" style="object-fit: cover;" alt="Jumeokbap">
-        </div>
-        <h6 class="text-center mt-2">Rice Balls</h6>
+      <div class="col-8"> 
+        <?php include "recipes.php" ?>
       </div>
-      
-      <div class="col-sm">
-        <div class="ratio ratio-1x1">
-          <img class="p-<?= $padding ?>" src="https://tiffycooks.com/wp-content/uploads/2021/04/Screen-Shot-2021-04-07-at-1.01.41-AM.png" style="object-fit: cover;" alt="Jumeokbap">
-        </div>
-        <h6 class="text-center mt-2">Marinated Eggs</h6>
+
+      <div class="col-2"> 
+        <h6>Filter by</h6>
+        <form action="/action_page.php">
+          <input type="checkbox" id="rice" name="rice" value="TOfu">
+          <label for="">Rice</label><br>
+          <input type="checkbox" id="tofu" name="tofu" value="Tofu">
+          <label for="">Tofu</label><br>
+          <input type="checkbox" id="eggs" name="E" value="Boat">
+          <label for="">Eggs</label><br><br>
+          <input type="submit" value="Submit">
+        </form>
       </div>
-      </div>
-    </div>
-    <div class="row gy-5">
-      <!-- add more imgs here! -->
     </div>
   </div>
 
