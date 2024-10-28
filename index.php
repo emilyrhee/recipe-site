7797a5dc841a50b60c790377e73a4c5602a3e5f1
emilyrhee <emilyrhee99@gmail.com>
2024-10-28 14:07:42 -0400

    more inputs for filter menu

--- index.php
+++ index.php
@@ -27,13 +27,19 @@
         <h5>Filter by</h5>
         <h6>Ingredients</h6>
         <form action="/action_page.php">
-          <input type="checkbox" id="rice" name="rice" value="Tofu">
-          <label for="">Rice</label><br>
-          <input type="checkbox" id="tofu" name="tofu" value="Tofu">
-          <label for="">Tofu</label><br>
-          <input type="checkbox" id="eggs" name="E" value="Boat">
-          <label for="">Eggs</label><br><br>
+          <input type="checkbox" id="rice" name="ingredients" value="Rice">
+          <label for="Rice">Rice</label><br>
+          <input type="checkbox" id="tofu" name="ingredients" value="Tofu">
+          <label for="Tofu">Tofu</label><br>
+          <input type="checkbox" id="eggs" name="ingredients" value="Eggs">
+          <label for="Eggs">Eggs</label><br><br>
+          <h6>Culture</h6>
+          <input type="checkbox" id="korean" name="culture" value="korean">
+          <label for="korean">Korean</label><br>
+          <input type="checkbox" id="korean" name="culture" value="korean">
+          <label for="korean">Japanese</label><br><br>
           <input type="submit" value="Submit">
+
         </form>
       </div>
     </div>
