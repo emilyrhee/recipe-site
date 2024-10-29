7e49577369a5a4c9b86bd3f738e50d19bd40eae6
emilyrhee <emilyrhee99@gmail.com>
2024-10-28 23:27:49 -0400

    made nav bar responsive for mobile
    added hamburger menu

--- components/navbar.php
+++ components/navbar.php
@@ -1,17 +1,26 @@
-<nav class="navbar">
+<nav class="navbar navbar-expand-lg navbar">
   <div class="container-fluid">
     <a class="navbar-brand">EasyBytes</a>
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
+    
+    <!-- Hamburger menu (for mobile) -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-    <div class="SignOut">
-    <span class="profile-icon">
-      <img class = "icon" src="../Images/profileIcon.png" alt="Profile Icon"/>
-    </span>
-   <a href="../components/LoginForm.php" title="Make an account and Sign-In"  style="text-decoration: none;">Login</a>
-  </div>
-
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="navbarContent">
+      <form class="d-flex ms-auto my-2 my-lg-0" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      
+      <!-- Login  -->
+      <div class="SignOut ms-lg-3 d-flex align-items-center">
+        <span class="profile-icon me-2">
+          <img class="icon" src="../Images/profileIcon.png" alt="Profile Icon" style="width: 30px; height: 30px;"/>
+        </span>
+        <a href="../components/LoginForm.php" title="Make an account and Sign-In" class="btn btn-outline-primary">Login</a>
+      </div>
+    </div>
   </div>
 </nav>
