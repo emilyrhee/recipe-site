1a265a43cd07a9b662574dea280364d1abe7a503
emilyrhee <emilyrhee99@gmail.com>
2024-10-31 14:22:45 -0400

    made form look nicer + added navbar

--- components/LoginForm.php
+++ components/LoginForm.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <style>
-    <?php include "styles/styles.css"?>
-  </style>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>    
-    <div class="login-container">
-        <h2 class="Login-txt">Login</h2>
+      <?php include "../styles/styles.css" ?>
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
 
-        <form class="login-form" method="POST" action="../components/UserLogin.php">
-            <div class="email-container">
-                <input class="email-txt" type="text" name="email" placeholder="Email" id="email" required>
-            </div>
+  <body>
+    <?php include "navbar.php"; ?>
 
-            <div class="psswd-container">
-                <input class="psswd-txt" type="password" name="password" placeholder="Password" id="password" required>
-            </div>
+    <div class="d-flex flex-column justify-content-center align-items-center pt-5">
+      <h2 class="Login-txt pb-3">Login</h2>
 
-            <input class="submitBtt" type="submit" placeholder="Submit" id="submit">
-        </form>
+      <form class="login-form" method="POST" action="../components/UserLogin.php">
+        <div class="email-container">
+          <input class="email-txt" type="text" name="email" placeholder="Email" id="email" required>
+        </div>
+
+        <div class="psswd-container py-3">
+          <input class="psswd-txt" type="password" name="password" placeholder="Password" id="password" required>
+        </div>
+
+        <input class="btn btn-primary" type="submit" placeholder="Submit" id="submit">
+      </form>
     </div>
-</body>
+  </body>
 </html>
