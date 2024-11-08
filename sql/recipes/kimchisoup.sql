a92b7186f13bedff112c3f25d8022b2b60dbbe76
emilyrhee <emilyrhee99@gmail.com>
2024-11-07 23:03:18 -0500

    SQL entry for kimchi soup recipe

---
+++ sql/recipes/kimchisoup.sql
@@ -0,0 +1,8 @@
+INSERT INTO Recipe (title, ingredients, instructions, category, chef_id)
+VALUES (
+    'Kimchi Soup',
+    'Kimchi,Pork Belly,Broth,Onion,Tofu,Gochugaru,Gochujang,Sesame Oil',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'Korean',
+    1
+);
