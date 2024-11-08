683b7c1e6e3701d4b5fb1b7b35013a52075e91f0
emilyrhee <emilyrhee99@gmail.com>
2024-11-08 14:13:17 -0500

    connected individual recipe page to database

--- components/connect.php
+++ components/connect.php
@@ -8,7 +8,12 @@
 
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
+        $conn->exec("USE recipemanagementsystem");
+        $stmt = $conn->prepare("SELECT title FROM Recipe WHERE id = :id");
+        $stmt->bindParam(':id', $recipeId);
+        $recipeId = 1; // this is hard-coded rn, needs to be changed later.
+        $stmt->execute();
+        $recipe = $stmt->fetch(PDO::FETCH_ASSOC);
 
     }catch(PDOException $e){
         echo "Connection to database have failed " . $e->getMessage() . "<br>";
---
+++ recipes/kimchisoup.php
@@ -0,0 +1,40 @@
+<?php include "../components/connect.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    <?php include "../styles/styles.css" ?>
+  </style>
+  <title>Kimchi Soup Recipe</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+  <?php include "../components/navbar.php"; ?>
+
+ <div class="container">
+    <div class="row">
+      <div class="col-2">
+        <?php include "../components/sidebar.php" ?>
+      </div>
+
+      <div class="col-8"> 
+        <?php
+          if (isset($recipe['title'])) {
+              echo "<h1>" . htmlspecialchars($recipe['title']) . "</h1>";
+          } else {
+              echo "<h1>Recipe not found</h1>";
+          }
+        ?>
+      </div>
+
+      <div class="col-2"> 
+        <!-- maybe put something here  -->
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
