6b4485f440a5b1e285e4e696b8c715a01ed1d698
emilyrhee <emilyrhee99@gmail.com>
2024-11-09 12:58:36 -0500

    dynamically change the page title based on recipe title in db

--- recipes/kimchisoup.php
+++ recipes/kimchisoup.php
@@ -7,6 +7,7 @@
   $recipeId = 1; // this is hard-coded rn, needs to be changed later.
   $stmt->execute();
   $recipe = $stmt->fetch(PDO::FETCH_ASSOC);
+  $pageTitle = htmlspecialchars($recipe['title']) . " Recipe";
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +19,7 @@
     <?php include "../styles/styles.css" ?>
   </style>
   <script src="../scripts/MyAccountScript.js" defer></script>
-  <title>Kimchi Soup Recipe</title>
+  <title><?php echo $pageTitle; ?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
