7035f1cfef055e5922df67f29bcf1e606d2b4ec5
emilyrhee <emilyrhee99@gmail.com>
2024-11-09 09:31:06 -0500

    sidebar buttons and login session starts

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,7 +1,9 @@
 <?php include __DIR__ . "/../AccountHamburger/MyAccount.php"; ?>
 <?php if(isset($_SESSION["user_id"])) : ?>
   <button class="btn btn-secondary my-1" id="checking" onclick="hamburgerDropDown()">My Account</button>
-  <a href="/handlers/logout.php" class="btn btn-secondary">Logout</a>
+  <div>
+    <a href="/handlers/logout.php" class="btn btn-secondary">Logout</a>
+  </div>
 <?php else : ?>
   <div>
     <a href="/LoginForm.php" class="btn btn-secondary my-1">Login</a>
