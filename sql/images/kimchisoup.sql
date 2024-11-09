521451295473680da48a278bf7a1c0aec1008cfc
emilyrhee <emilyrhee99@gmail.com>
2024-11-09 13:13:00 -0500

    added kimchi soup image db entry

---
+++ sql/images/kimchisoup.sql
@@ -0,0 +1,6 @@
+INSERT INTO RecipeImages (recipe_id, image_url, description)
+VALUES (
+    1,
+    'https://www.maangchi.com/wp-content/uploads/2007/11/kimchijjigae-590x411.jpg',
+    'Final result of kimchi soup'
+);
