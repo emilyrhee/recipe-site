bb464fc2b5eddd001776fb2c65f29e3aa1f9b52e
emilyrhee <emilyrhee99@gmail.com>
2024-11-09 12:14:18 -0500

    moved dropdown links below button

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,6 +1,6 @@
-<?php include __DIR__ . "/../AccountHamburger/MyAccount.php"; ?>
 <?php if(isset($_SESSION["user_id"])) : ?>
-  <button class="btn btn-secondary my-1" id="checking" onclick="hamburgerDropDown()">My Account</button>
+  <button class="btn btn-secondary my-1" id="checking" onclick="hamburgerDropDown()">My Account ▾</button>
+  <?php include __DIR__ . "/../AccountHamburger/MyAccount.php"; ?>
   <div>
     <a href="/handlers/logout.php" class="btn btn-secondary">Logout</a>
   </div>
