d66ab872a065c5bca1b338b153ad986d0c98fa66
emilyrhee <emilyrhee99@gmail.com>
2024-11-13 20:01:09 -0500

    change title from default to "My Recipes"

--- ChefSide/chef_recipes_display.php
+++ ChefSide/chef_recipes_display.php
@@ -40,7 +40,7 @@ if (!empty($errorMessage)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>My Recipes</title>
 </head>
 <body>
     <div>
