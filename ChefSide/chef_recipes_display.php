c191a6542e6c5d12090398b64762c79745e7f2f7
emilyrhee <emilyrhee99@gmail.com>
2024-11-15 22:54:35 -0500

    improved frontend
    added sidebar, navbar, implemented columns to center content

--- ChefSide/chef_recipes_display.php
+++ ChefSide/chef_recipes_display.php
@@ -27,38 +27,55 @@ if (isset($conn)) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>My Recipes</title>
+  <style>
+    <?php include "../styles/styles.css" ?>
+  </style>
+  <script src="../scripts/MyAccountScript.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-  <div>
-    <?php
-    if (!empty($errorMessage)) {
-        echo "<p class='error'>" . htmlspecialchars($errorMessage) . "</p>";
-    } else {
-        foreach ($recipes as $recipe) {
-            $title = htmlspecialchars($recipe['title']);
-            $category = htmlspecialchars($recipe['category']);
-            $ingredients = htmlspecialchars($recipe['ingredients']);
-            $instructions = htmlspecialchars($recipe['instructions']);
-            $imageUrl = !empty($recipe['image_url']) ? htmlspecialchars($recipe['image_url']) : 'default-image.jpg'; // Fallback image if URL is missing
+  <?php include "../components/navbar.php"; ?>
+  <div class="container">
+    <div class="row pt-4">
+      <div class="col-2">
+        <?php include "../components/sidebar.php"; ?>
+      </div>
 
-            echo "
-            <div class='recipe'>
-                <h3>{$title}</h3>
-                <p><em>Category:</em> {$category}</p>
-                <p><em>Ingredients:</em> {$ingredients}</p>
-                <p><em>Instructions:</em> {$instructions}</p>
-                <div class='image-container'>
-                    <img src='{$imageUrl}' class='pic-of-recipes' alt='Image of {$title}' style='width: 200px;'>
-                </div>
-            </div>";
-        }
-    }
-    ?>
+      <div class="col-8">
+        <?php if (!empty($errorMessage)): ?>
+          <p class="error"><?= htmlspecialchars($errorMessage) ?></p>
+        <?php else: ?>
+          <?php foreach ($recipes as $recipe): ?>
+            <?php
+              $title = htmlspecialchars($recipe['title']);
+              $category = htmlspecialchars($recipe['category']);
+              $ingredients = htmlspecialchars($recipe['ingredients']);
+              $instructions = htmlspecialchars($recipe['instructions']);
+              $imageUrl = !empty($recipe['image_url']) ? htmlspecialchars($recipe['image_url']) : 'default-image.jpg';
+            ?>
+            <div class="recipe">
+              <h3><?= $title ?></h3>
+              <p>Category: <?= $category ?></p>
+              <p>Ingredients: <?= $ingredients ?></p>
+              <p>Instructions: <?= $instructions ?></p>
+              <img src="<?= $imageUrl ?>" alt="Image of <?= $title ?>" class="pb-5 d-block" style="width: 200px;">
+            </div>
+          <?php endforeach; ?>
+        <?php endif; ?>
 
-    <a href="../ChefScreen.php"><button class="btn btn-primary">Add Recipe</button></a>
+        <!-- TODO: make this a part of the side bar only when you're on this page -->
+        <a href="../ChefScreen.php">
+          <button class="btn btn-primary">Add Recipe</button>
+        </a>
+      </div>
+
+      <div class="col-2"></div>
+    </div>
   </div>
+
+  <?php include "../components/footer.php" ?>
 </body>
+
 
 </html>
