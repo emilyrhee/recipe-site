7c98b64d5d548f7f3ff18c247936fce8530b6268
emilyrhee <emilyrhee99@gmail.com>
2024-11-17 15:00:39 -0500

    separated filter menu into a component

---
+++ components/filter.php
@@ -0,0 +1,16 @@
+<h5>Filter by</h5>
+<h6>Ingredients</h6>
+<form action="/action_page.php">
+  <input type="checkbox" id="rice" name="ingredients" value="Rice">
+  <label for="Rice">Rice</label><br>
+  <input type="checkbox" id="tofu" name="ingredients" value="Tofu">
+  <label for="Tofu">Tofu</label><br>
+  <input type="checkbox" id="eggs" name="ingredients" value="Eggs">
+  <label for="Eggs">Eggs</label><br><br>
+  <h6>Culture</h6>
+  <input type="checkbox" id="korean" name="culture" value="korean">
+  <label for="korean">Korean</label><br>
+  <input type="checkbox" id="korean" name="culture" value="korean">
+  <label for="korean">Japanese</label><br><br>
+  <input type="submit" value="Submit">
+</form>
--- index.php
+++ index.php
@@ -31,24 +31,7 @@ session_start();
         <?php include "components/recipes.php" ?>
       </div>
 
-      <div class="col-2">
-        <h5>Filter by</h5>
-        <h6>Ingredients</h6>
-        <form action="/action_page.php">
-          <input type="checkbox" id="rice" name="ingredients" value="Rice">
-          <label for="Rice">Rice</label><br>
-          <input type="checkbox" id="tofu" name="ingredients" value="Tofu">
-          <label for="Tofu">Tofu</label><br>
-          <input type="checkbox" id="eggs" name="ingredients" value="Eggs">
-          <label for="Eggs">Eggs</label><br><br>
-          <h6>Culture</h6>
-          <input type="checkbox" id="korean" name="culture" value="korean">
-          <label for="korean">Korean</label><br>
-          <input type="checkbox" id="korean" name="culture" value="korean">
-          <label for="korean">Japanese</label><br><br>
-          <input type="submit" value="Submit">
-
-        </form>
+        <?php include "components/filter.php" ?>
       </div>
     </div>
   </div>
