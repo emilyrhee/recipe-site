f5bc0440ba30a8d8823a78f9afda29674adca6a4
emilyrhee <emilyrhee99@gmail.com>
2024-11-21 17:51:38 -0500

    made search a dropdown

--- components/navbar.php
+++ components/navbar.php
@@ -1,8 +1,7 @@
-
 <nav class="navbar navbar-expand-lg navbar">
   <div class="container-fluid">
     <a class="navbar-brand" href="/index.php">EasyBytes</a>
-    
+
     <!-- Hamburger menu (for mobile) -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -10,31 +9,18 @@
 
     <!-- Collapsible content -->
     <div class="collapse navbar-collapse" id="navbarContent">
-      <form class="d-flex ms-auto my-2 my-lg-0" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" id="searching" aria-label="Search" onkeyup="search()">
+      <form class="d-flex ms-auto my-2 my-lg-0" role="search" style="position: relative;">
+        <input class="form-control me-2" type="search" placeholder="Search" id="searching" aria-label="Search">
         <button class="btn btn-outline-success" type="submit" onclick="search()">Search</button>
-        <div class="result" id="result"></div>
-        <?php if(isset($_SESSION["user_id"])): ?>
-          <div>
+        <div id="result" class="dropdown-results"></div>
+      </form>
+
+      <?php if (isset($_SESSION["user_id"])): ?>
+        <div>
           <span>
             <?php echo htmlspecialchars($_SESSION["username"]); ?>
           </span>
-          </div>
-        <?php endif; ?>
-        
-        <div >
-      </form>
-      
-      <!-- Login  -->
-      <!-- <div class="SignOut ms-lg-3 d-flex align-items-center">
-        <span class="profile-icon me-2">
-          <img class="icon" src="../Images/profileIcon.png" alt="Profile Icon" style="width: 30px; height: 30px;"/>
-        </span>
-        <a href="../components/LoginForm.php" title="Make an account and Sign-In" class="btn btn-outline-primary">Login</a>
-      </div> -->
-      <!-- login/logout logic --->
-
-
+        </div>
+      <?php endif; ?>
     </div>
-  </div>
 </nav>
--- index.php
+++ index.php
@@ -15,25 +15,6 @@ include "./handlers/pagination_logic.php";
   <script src="./scripts/script.js" defer></script>
   <script src="./scripts/MyAccountScript.js"></script>
   <script src="./scripts/bookmark.js"></script>
-  <script>
-    function search() {
-      var query = new FormData();
-      query.append('searching', document.getElementById("searching").value);
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', '../UserFeatures/recipe_title_storage.php');
-      xhr.onload = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          document.getElementById("result").innerHTML = xhr.responseText;
-        }
-      };
-      xhr.send(query);
-    }
-
-    document.addEventListener('DOMContentLoaded', function() {
-      const searchInput = document.getElementById('searching');
-      searchInput.addEventListener('keyup', search);
-    });
-  </script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
