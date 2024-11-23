d8421665f63346a06551477fa12ac92a001f3f8f
emilyrhee <emilyrhee99@gmail.com>
2024-11-22 19:06:52 -0500

    refactor: moved search to its own js file

--- index.php
+++ index.php
@@ -12,9 +12,10 @@ include "./handlers/pagination_logic.php";
   <style>
     <?php include "styles/styles.css" ?>
   </style>
-  <script src="./scripts/script.js" defer></script>
-  <script src="./scripts/MyAccountScript.js"></script>
-  <script src="./scripts/bookmark.js"></script>
+  <script src="scripts/script.js"></script>
+  <script src="scripts/MyAccountScript.js"></script>
+  <script src="scripts/bookmark.js"></script>
+  <script src="scripts/search.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
---
+++ scripts/search.js
@@ -0,0 +1,21 @@
+function search() {
+  var query = new FormData();
+  query.append('searching', document.getElementById("searching").value);
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '../UserFeatures/recipe_title_storage.php');
+  xhr.onload = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      const resultDiv = document.getElementById("result");
+      resultDiv.innerHTML = xhr.responseText.trim();
+
+      resultDiv.classList.add("active");
+    }
+    document.addEventListener('click', function (e) {
+      const resultDiv = document.getElementById('result');
+      if (!e.target.closest('form')) {
+        resultDiv.classList.remove('active');
+      }
+    });
+  };
+  xhr.send(query);
+}
