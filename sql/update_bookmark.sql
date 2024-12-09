dce93a43027046f272eec0fafe377ac3bfaf230b
emilyrhee <emilyrhee99@gmail.com>
2024-12-08 21:34:26 -0500

    fix: update Bookmark table to allow for deleting users as admin

--- sql/update_bookmark.sql
+++ sql/update_bookmark.sql
@@ -8,3 +8,9 @@ ADD CONSTRAINT fk_bookmark_recipe
 FOREIGN KEY (recipe_id)
 REFERENCES Recipe(id)
 ON DELETE CASCADE;
+
+ALTER TABLE Bookmark
+DROP FOREIGN KEY Bookmark_ibfk_2;
+
+ALTER TABLE Bookmark
+ADD CONSTRAINT Bookmark_ibfk_2 FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE;
