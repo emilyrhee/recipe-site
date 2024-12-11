eaa95e2110f5a22ff3c9eb31ae8f23e5749cc225
PersonalMaryJane <personaljproj05@gmail.com>
2024-12-11 10:09:00 -0500

    got a working update profile

--- handlers/profile.php
+++ handlers/profile.php
@@ -27,4 +27,61 @@ if(isset($conn)){
         $error_message = "Have error connecting to the database " . $e->getMessage();
     }
 }
+
+if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SESSION['user_id'])){
+
+    $username = htmlspecialchars(trim($_POST['username']));
+    $email = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
+    $password = htmlspecialchars(trim($_POST['password']));
+    //$role = htmlspecialchars(trim($_POST['role'])); //we don't need this since it doesn't get updated.
+    $phone = htmlspecialchars(trim($_POST['phone']));
+    //$user_id = $_SESSION['user_id'];
+
+    if(isset($conn)){
+        $db = "recipemanagementsystem";
+
+        try{
+            $udpat_sql = "UPDATE Users SET username = :username, email = :email, phone = :phone";
+
+            // check if the password is empty
+
+            if(!empty($password)){
+                $udpat_sql .= ", password = :password";
+                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            }
+            
+            $udpat_sql .= " WHERE id = :user_id";
+
+            $update_query = $conn->prepare($udpat_sql);
+
+            $update_query->bindParam(":username", $username);
+            $update_query->bindParam(":email", $email);
+            $update_query->bindParam(":phone", $phone);
+            $update_query->bindParam(":user_id", $user_id);
+
+            if(!empty($password)){
+                $update_query->bindParam(":password", $password);
+            }
+            // try to execute so that we let users know they updated successfully.
+
+            if($update_query->execute()){
+                echo "Successfully Updated your profile";
+                $_SESSION['username'] = $username;
+
+                header("Location: .././index.php " );
+            }else{
+                echo "Failed to update your code.";
+            }
+
+
+        }catch(PDOException $e){
+            $error = "Error connecting to the database " . $e->getMessage();
+            echo $error;
+        }
+    }
+
+    ///echo "HHHHHHHHHHH";
+    
+    //var_dump($username);
+}
 ?>
