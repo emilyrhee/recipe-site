97db0ecfc733cd3373698282b1c7fed81c73e1e7
emilyrhee <emilyrhee99@gmail.com>
2024-12-10 21:52:29 -0500

    chore: added Profile page to admin's drop down

--- AccountHamburger/MyAccount.php
+++ AccountHamburger/MyAccount.php
@@ -14,7 +14,8 @@ if (isset($_SESSION["role"]) && $_SESSION['role'] === 'client') : ?>
   </div>
 <?php elseif (isset($_SESSION["role"]) && $_SESSION['role'] === 'admin') : ?>
   <div id="MyAccountDropdown" style="display: none;">
-    <li style="list-style: none;"><a href="Admin/Admin.php" style="text-decoration: none;">Admin Page</a></li>
+    <li style="list-style: none;"><a href="../UserFeatures/profile_screen.php" style="text-decoration: none;">Profile</a></li>
+    <li style="list-style: none;"><a href="Admin/Admin.php" style="text-decoration: none;">Manage Users</a></li>
     <li style="list-style: none;"><a href="../handlers/logout.php">Logout</a></li>
   </div>
 <?php endif; ?>
